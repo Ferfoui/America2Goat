510269483d11cd0e1fb77a8420e69495015d8acb
Ferfoui <kenji.oku@outlook.fr>
2024-09-24 20:55:26 +0200

    Add a link to the repository

--- app/src/main/java/fr/ferfoui/america2goat/ui/appinfo/AppInfoFragment.java
+++ app/src/main/java/fr/ferfoui/america2goat/ui/appinfo/AppInfoFragment.java
@@ -1,15 +1,26 @@
 package fr.ferfoui.america2goat.ui.appinfo;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import fr.ferfoui.america2goat.R;
 import fr.ferfoui.america2goat.databinding.FragmentAppInfoBinding;
 
 public class AppInfoFragment extends Fragment {
@@ -32,11 +43,44 @@ public class AppInfoFragment extends Fragment {
         return binding.getRoot();
     }
 
-    // TODO: Use a ClickableSpan to open the app's GitHub page in a browser instead of a TextView
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        configureGithubLink();
+    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @SuppressLint("ResourceType")
+    private void configureGithubLink() {
+        TextView devGithubText = binding.devGithubText;
+
+        String text = getString(R.string.repository_text);
+        SpannableString spannableString = new SpannableString(text);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                openGithubRepository();
+            }
+        };
+
+        // Apply styles
+        spannableString.setSpan(clickableSpan, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.light_purple_for_dark_mode, null)), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new UnderlineSpan(), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        devGithubText.setText(spannableString);
+        devGithubText.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void openGithubRepository() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.repository_url)));
+        startActivity(browserIntent);
     }
 }
